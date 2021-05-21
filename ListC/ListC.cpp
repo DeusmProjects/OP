bcbe3b0f5fb865f8c6df78be001dc044ea7eac57
deusmemes <maxin29999@gmail.com>
2021-05-21 17:54:13 +0400

    Добавьте файлы проекта.

---
+++ ListC/ListC.cpp
@@ -0,0 +1,173 @@
+﻿// ListC.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+
+typedef struct _Node {
+    int value;
+    struct _Node* next = nullptr;
+    struct _Node* prev = nullptr;
+} Node;
+
+
+typedef struct _List {
+    size_t size = 0;
+    Node* head = nullptr;
+    Node* tail = nullptr;
+} List;
+
+
+List* createList() {
+    List* tmp = (List*)malloc(sizeof(List));
+
+    return tmp;
+}
+
+
+void pushFront(List* list, int data) {
+    Node* tmp = (Node*)malloc(sizeof(Node));
+    if (tmp == NULL) {
+        exit(1);
+    }
+    tmp->value = data;
+    tmp->next = list->head;
+    tmp->prev = NULL;
+    if (list->head) {
+        list->head->prev = tmp;
+    }
+    list->head = tmp;
+
+    if (list->tail == NULL) {
+        list->tail = tmp;
+    }
+    list->size++;
+}
+
+
+int popFront(List* list) {
+    Node* prev;
+    int tmp;
+    if (list->head == NULL) {
+        exit(2);
+    }
+
+    prev = list->head;
+    list->head = list->head->next;
+    if (list->head) {
+        list->head->prev = NULL;
+    }
+    if (prev == list->tail) {
+        list->tail = NULL;
+    }
+    tmp = prev->value;
+    free(prev);
+
+    list->size--;
+    return tmp;
+}
+
+
+void pushBack(List* list, int value) {
+    Node* tmp = (Node*)malloc(sizeof(Node));
+    if (tmp == NULL) {
+        exit(3);
+    }
+    tmp->value = value;
+    tmp->next = NULL;
+    tmp->prev = list->tail;
+    if (list->tail) {
+        list->tail->next = tmp;
+    }
+    list->tail = tmp;
+
+    if (list->head == NULL) {
+        list->head = tmp;
+    }
+    list->size++;
+}
+
+int popBack(List* list) {
+    Node* next;
+    int tmp;
+    if (list->tail == NULL) {
+        exit(4);
+    }
+
+    next = list->tail;
+    list->tail = list->tail->prev;
+    if (list->tail) {
+        list->tail->next = NULL;
+    }
+    if (next == list->head) {
+        list->head = NULL;
+    }
+    tmp = next->value;
+    free(next);
+
+    list->size--;
+    return tmp;
+}
+
+
+Node* getNth(List* list, size_t index) {
+    Node* tmp = list->head;
+    size_t i = 0;
+
+    while (tmp && i < index) {
+        tmp = tmp->next;
+        i++;
+    }
+
+    return tmp;
+}
+
+void insert(List* list, size_t index, int value) {
+    Node* elm = NULL;
+    Node* ins = NULL;
+    elm = getNth(list, index);
+    if (elm == NULL) {
+        exit(5);
+    }
+    ins = (Node*)malloc(sizeof(Node));
+    ins->value = value;
+    ins->prev = elm;
+    ins->next = elm->next;
+    if (elm->next) {
+        elm->next->prev = ins;
+    }
+    elm->next = ins;
+
+    if (!elm->prev) {
+        list->head = elm;
+    }
+    if (!elm->next) {
+        list->tail = elm;
+    }
+
+    list->size++;
+}
+
+
+void printDblLinkedList(List* list, void (*fun)(int)) {
+    Node* tmp = list->head;
+    while (tmp) {
+        fun(tmp->value);
+        tmp = tmp->next;
+    }
+    printf("\n");
+}
+
+
+void printInt(int value) {
+    printf("%d ", value);
+}
+
+int main()
+{
+    List* list = createList();
+    pushBack(list, 1);
+    pushBack(list, 2);
+    pushBack(list, 3);
+    printDblLinkedList(list, printInt);
+}
---
+++ OP/OP.cpp
@@ -0,0 +1,185 @@
+﻿#include <iostream> 
+
+//using namespace std;
+
+struct Item
+{
+    int id;
+    char name[10];
+};
+
+struct Node {
+    struct Item* item = nullptr;
+    struct Node* next = nullptr;
+    struct Node* prev = nullptr;
+};
+
+struct List {
+    unsigned int size = 0;
+    struct Node* head = nullptr;
+    struct Node* tail = nullptr;
+};
+
+Node* getNode(List* list, int index) {
+    if (index >= list->size) {
+        exit(5);
+    }
+
+    int halfSize = list->size / 2;
+    int i;
+    Node* current;
+
+    if (index < halfSize) {
+        i = 0;
+        current = list->head;
+
+        while (current != nullptr && i < index) {
+            current = current->next;
+            i++;
+        }
+    }
+    else {
+        i = list->size - 1;
+        current = list->tail;
+
+        while (current != nullptr && i > index) {
+            current = current->prev;
+            i--;
+        }
+    }
+
+    return current;
+}
+
+void insertNodeBefore(List* list, Item* item, int index) {
+    Node* newNode = new Node;
+    newNode->item = item;
+
+    Node* element = getNode(list, index);
+    if (element == nullptr) {
+        exit(7);
+    }
+
+    newNode->next = element;
+    newNode->prev = element->prev;
+
+    if (element->prev != nullptr) {
+        element->prev->next = newNode;
+    }
+
+    element->prev = newNode;
+
+    if (newNode->prev == nullptr) {
+        list->head = newNode;
+    }
+
+    list->size++;
+}
+
+void insertNodeAfter(List* list, Item* item, int index) {
+    Node* newNode = new Node;
+    newNode->item = item;
+
+    Node* element = getNode(list, index);
+    if (element == nullptr) {
+        exit(6);
+    }
+
+    newNode->prev = element;
+    newNode->next = element->next;
+
+    if (element->next != nullptr) {
+        element->next->prev = newNode;
+    }
+
+    element->next = newNode;
+
+    if (newNode->next == nullptr) {
+        list->tail = newNode;
+    }
+
+    list->size++;
+}
+
+void addNode(List* list, Item* item)
+{
+    if (list->size == 0)
+    {
+        Node* newNode = new Node;
+        newNode->item = item;
+        list->head = newNode;
+        list->tail = newNode;
+        list->size++;
+    }
+    else
+    {
+        insertNodeAfter(list, item, list->size - 1);
+    }
+}
+
+void sortedInsert(List* list, Item* item) {
+    Node* newNode = new Node;
+    newNode->item = item;
+    Node* current;
+
+    if (list->size == 0) {
+        addNode(list, item);
+    }
+    else {
+        int index = -1;
+        current = list->head;
+
+        /*bool conditionInt = current->item->id < item->id;
+        bool conditionStr = strcmp(current->item->name, item->name);*/
+
+        if (strcmp(current->item->name, item->name) >= 0) {
+            insertNodeBefore(list, item, 0);
+            return;
+        }
+
+        if (strcmp(list->tail->item->name, item->name) == -1) {
+            insertNodeAfter(list, item, list->size - 1);
+            return;
+        }
+
+        while (current != nullptr && strcmp(current->item->name, item->name) == -1) {
+            current = current->next;
+            index++;
+        }
+
+        insertNodeAfter(list, item, index);
+    }
+}
+
+void printList(List* list)
+{
+    Node* tmp = list->head;
+    for (int i = 0; i < list->size; ++i)
+    {
+        printf("item %d - name: %s\n", tmp->item->id, tmp->item->name);
+        tmp = tmp->next;
+    }
+}
+
+int main()
+{
+    List* list = new List;
+
+    sortedInsert(list, new Item{ 1, "ertt" });
+    sortedInsert(list, new Item{ 3, "adwaw" });
+    sortedInsert(list, new Item{ 2, "cxccc" });
+
+    /*addNode(list, new Item{1, "aaa"});
+    addNode(list, new Item{2, "bbb"});
+    addNode(list, new Item{3, "ccc"});
+    addNode(list, new Item{4, "ddd"});
+    insertNodeBefore(list, new Item{ 123, "awdawdaw" }, 0);
+    insertNodeAfter(list, new Item{5, "eee"}, 1);*/
+    //insertNodeBefore(list, new Item{6, "fff"}, 4);
+    //insertNodeBefore(list, new Item{7, "ffdaff"}, 5);
+    //insertNodeAfter(list, new Item{11, "fawdaff"}, 3);
+
+    printList(list);
+
+    return 0;
+}
